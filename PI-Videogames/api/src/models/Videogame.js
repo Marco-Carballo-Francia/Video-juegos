4b167d4834aec48be2d051033b6e29811645648c
Marco-Carballo-Francia <solojuegos.kiku2020@gmail.com>
2021-08-06 13:20:29 -0300

    created DataBase

--- PI-Videogames/api/src/models/Videogame.js
+++ PI-Videogames/api/src/models/Videogame.js
@@ -4,9 +4,42 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dates: {
+      type: DataTypes.STRING,
+      defaultValue:DataTypes.DATE,
+      allowNull: true
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    platforms: {
+      type: DataTypes.JSON,
+      allowNull: false
+    }
   });
 };
+
+
+
+// [ ] Videojuego con las siguientes propiedades:
+// ID: * No puede ser un ID de un videojuego ya existente en la API rawg
+// Nombre *
+// Descripción *
+// Fecha de lanzamiento
+// Rating
+// Plataformas *
